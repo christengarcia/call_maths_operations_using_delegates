4bd2e13015ab41eea813be0c9bc2140f099e697c
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-14 15:57:12 +1100

    Add class Application

--- call_maths_operations_using_delegates/Program.cs
+++ call_maths_operations_using_delegates/Program.cs
@@ -16,3 +16,26 @@ static class MathOperations
         return value * value;
     }
 }
+
+delegate double DoubleOp(double x);
+
+class Application
+{
+    static void Main()
+    {
+        DoubleOp[] operations =
+            {
+               MathOperations.Multiply,
+               MathOperations.Square
+            };
+
+        for (int i = 0; i < operations.Length; i++)
+        {
+            Console.WriteLine("Operation[{0}]:", i);
+            ProcessAndDisplayNumber(operations[i], 5.0);
+            ProcessAndDisplayNumber(operations[i], 13.55);
+            ProcessAndDisplayNumber(operations[i], 1.732);
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
