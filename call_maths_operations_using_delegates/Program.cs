44335c56a1518c0ea4f8ecf2ccac92869455a765
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-01-14 15:53:57 +1100

    Initial commit

---
+++ call_maths_operations_using_delegates/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ *  C# Program to Call Math Operations using Delegates
+ */
+
+using System;
+
+static class MathOperations
+{
+    public static double Multiply(double value)
+    {
+        return value * 2;
+    }
+
+    public static double Square(double value)
+    {
+        return value * value;
+    }
+}
